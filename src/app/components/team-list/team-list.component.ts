330ba733a65d4aba479733b95e150d84fba4acd4
Andrew Palmer <andy@showcasetech.net>
2018-03-26 20:31:07 -0500

    team list component - maybe it works?

--- src/app/components/team-list/team-list.component.ts
+++ src/app/components/team-list/team-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Person } from '../../models/person';
+import { PersonParserService } from '../../services/person-parser.service';
+import { Team } from '../../models/team';
 
 @Component({
   selector: 'app-team-list',
@@ -8,10 +10,26 @@ import { Person } from '../../models/person';
 })
 export class TeamListComponent implements OnInit {
   @Input() people: Person[];
+  teams: Team[];
 
-  constructor() { }
+  constructor(private personParsingService: PersonParserService) { }
 
   ngOnInit() {
+    this.people = this.personParsingService.people;
+    this.teams = this.buildTeams(this.people)  
   }
-
+  buildTeams(people): Array<Team> {
+    let teams: Array<Team> = [];
+    people.forEach(element => {
+      var team = element.team;
+      let addTeam = true;
+      teams.forEach(t => {
+        if (t == team)
+          addTeam = false;
+      });
+      if (addTeam) 
+        teams.push(team);
+    });
+    return teams;
+  }
 }
