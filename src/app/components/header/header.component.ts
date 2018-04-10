71ef8cfd74f114c8452ba9a70873719c8000d411
Joseph <josephdanielbartley@gmail.com>
2018-04-09 19:06:09 -0500

    fixed import button click

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,19 +9,19 @@ export class HeaderComponent implements OnInit {
   title = 'app';
   valid = '';
   validation = '';
-  
 
 
-  constructor() { 
+
+  constructor() {
   }
 
   ngOnInit() {
   }
 
   checkExtension (val) {
-    var fileList = val.srcElement.files;
-    var fileName = fileList[0].name.split('.');
-    var extension = fileName[1];
+    let fileList = val.srcElement.files;
+    let fileName = fileList[0].name.split('.');
+    let extension = fileName[1];
     if (extension === 'csv') {
       this.valid = "Valid"
       this.validation = "green";
@@ -31,4 +31,36 @@ export class HeaderComponent implements OnInit {
     }
   }
 
+
+  openUpload(): void {
+    if (this.detectIE()) {
+      document.getElementById('srcfile').click();
+    }
+  }
+
+  detectIE() {
+    const ua = window.navigator.userAgent;
+
+    const msie = ua.indexOf('MSIE ');
+    if (msie > 0) {
+      // IE 10 or older => return version number
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    const trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+      // IE 11 => return version number
+      const rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    const edge = ua.indexOf('Edge/');
+    if (edge > 0) {
+      // Edge (IE 12+) => return version number
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
+
+    // other browser
+    return false;
+  }
 }
