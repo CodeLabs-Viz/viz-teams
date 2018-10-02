c19994e4f326a6df823286d18f461421519d2579
JosephDanielBartley <josephdanielbartley@gmail.com>
2018-10-02 10:36:19 -0500

    refactored header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,24 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { PersonParserService } from '../../services/person-parser.service';
-import { PersonService } from '../../services/person.service';
-import { Person } from '../../models/person';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-  title = 'app';
+export class HeaderComponent {
 
-  constructor(
-    private router: Router) {}
-
-  ngOnInit() {
-  }
-
-  home(): void {
-    this.router.navigateByUrl('/');
-  }
+  constructor() {}
 }
