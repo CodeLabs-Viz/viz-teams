c1859964968f2b8205789fd6d2e2aaa09dead587
JosephDanielBartley <josephdanielbartley@gmail.com>
2018-10-02 11:28:45 -0500

    yet another refactor

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
