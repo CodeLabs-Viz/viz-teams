486978067ff60caa9c1b188e7451f846e6a0b158
JosephDanielBartley <josephdanielbartley@gmail.com>
2018-10-24 18:09:52 -0500

    fixed detectIE to also detect Firefox

--- src/app/components/team-list/team-list.component.ts
+++ src/app/components/team-list/team-list.component.ts
@@ -78,31 +78,35 @@ export class TeamListComponent {
   }
 
   openUpload(): void {
-    if (this.detectIE()) {
+    if (this.detectIEorFirefox()) {
       document.getElementById('srcfile').click();
     }
   }
 
-  detectIE(): any {
+  detectIEorFirefox(): boolean {
     const ua = window.navigator.userAgent;
 
     const msie = ua.indexOf('MSIE ');
     if (msie > 0) {
       // IE 10 or older => return version number
-      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+      return true;
     }
 
     const trident = ua.indexOf('Trident/');
     if (trident > 0) {
       // IE 11 => return version number
-      const rv = ua.indexOf('rv:');
-      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+      return true;
     }
 
     const edge = ua.indexOf('Edge/');
     if (edge > 0) {
       // Edge (IE 12+) => return version number
-      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+      return true;
+    }
+
+    const firefox = ua.indexOf('Firefox/');
+    if (firefox > 0) {
+      return true;
     }
 
     // other browser
