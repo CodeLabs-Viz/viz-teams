4f7663a93709b2fb5bbddc05a1ea11f67f42ff12
MarkJRigdon <sendemailtomark@gmail.com>
2018-11-12 16:52:23 -0600

    add firebase apikey and authdomain to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Person } from './models/person';
 import { DragService } from './services/drag.service';
+import * as firebase from 'firebase' 
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,17 @@ import { DragService } from './services/drag.service';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   onDrop(data: any) {
     alert(`dropped: ${data}`);
+  }
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: 'AIzaSyBBFLTGAmv6yvIFns7H2XAKV5flYLEm-7U',
+      authDomain: 'viz-teams.firebaseapp.com'
+    });
   }
 
 }
