4ce9cf3d091f956be94f3f3d8f7a1a65c5324553
MarkJRigdon <sendemailtomark@gmail.com>
2018-11-13 13:47:08 -0600

    +logout() to app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Person } from './models/person';
 import { DragService } from './services/drag.service';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,12 @@ import { DragService } from './services/drag.service';
 })
 
 export class AppComponent implements OnInit {
+
+  constructor(private auth: AuthService){}
+
+  logout(){
+    this.auth.logout();
+  }
 
   onDrop(data: any) {
     alert(`dropped: ${data}`);
