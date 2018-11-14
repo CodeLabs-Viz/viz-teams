9e62d7f6908c33022af8592c3dcf1193b6e09103
MarkJRigdon <sendemailtomark@gmail.com>
2018-11-14 13:46:40 -0600

    logout, login, and signup redirects + router logic

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,10 +8,7 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-  constructor(private authService : AuthService, private router: Router) {}
+  constructor() {}
 
-  logOut() {
-    this.authService.logout
-    this.router.navigate(["/login"]) 
-  }
+
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,29 @@
 import {AngularFireAuth} from '@angular/fire/auth';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService { 
 
-  constructor(private afAuth : AngularFireAuth) {}
+  constructor(private afAuth : AngularFireAuth, private router: Router) {}
 
   signUp(email: string , password: string) {
     console.log('signUp called in auth.servie.ts');
     this.afAuth.auth.createUserWithEmailAndPassword(email , password).then(response => console.log(response)).catch(error => console.log(error));
+    this.router.navigate(["/home"]);
   }
 
   login(email: string , password: string){
     console.log('login called in auth.service.ts');
     this.afAuth.auth.signInWithEmailAndPassword(email , password).then(response => console.log(response)).catch(error => console.log(error));
+    this.router.navigate(["/home"]);
   }
 
   logout() {
     console.log('logout called in auth.service.ts');
     this.afAuth.auth.signOut().then(response => console.log(response)).catch(error => console.log(error));
+    this.router.navigate(["/login"]);
   }
 }
